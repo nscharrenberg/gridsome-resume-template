acb36df777cb95477dbec6280c3d9055d40126e6
Noah Scharrenberg <nscharrenberg@hotmail.com>
2021-01-31 15:04:39 +0100

    basic templates added

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,20 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: 'Noah Scharrenberg Portfolio',
+  siteUrl: 'https://nscharrenberg.github.io',
+  plugins: [
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-72659574-11'
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000
+      }
+    }
+  ]
+};
